0a9b6e9a5b7f011839eb3e7bc573fa064192a007
kaziabulhasib <abulhasibkazi@gmail.com>
2024-03-16 10:50:56 +0530

    header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className='text-4xl bg-red-500 text-white text-center'>
-        assignment-7-project
-      </h1>
-      <img src=' ' alt='' />
+      <Header></Header>
     </>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import { RiSearchLine } from "react-icons/ri";
+import { IoPersonCircleOutline } from "react-icons/io5";
+
+const Header = () => {
+  return (
+    <div className='flex justify-around items-center border'>
+      <div>
+        <h1 className='font-bold text-3xl text-[#150B2B]'>Recipe Calories</h1>
+      </div>
+      <div>
+        <ul className=' flex gap-12 text-[#150B2BB2] '>
+          <li>
+            <a href=''>Home</a>
+          </li>
+          <li>
+            <a href=''>Recipes</a>
+          </li>
+          <li>
+            <a href=''>About</a>
+          </li>
+          <li>
+            <a href=''>Search</a>
+          </li>
+        </ul>
+      </div>
+      <div className='flex gap-4 items-center relative '>
+        <RiSearchLine className='search-icon absolute left-6 top-[50] ' />
+        <input
+          className='bg-[#150B2B0D] border-none outline-none px-10 py-3 rounded-[48px] text-[#150B2BB2] '
+          type='text'
+          placeholder='  Search'
+        />
+
+        <IoPersonCircleOutline className='bg-green-500 text-3xl rounded-full' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
