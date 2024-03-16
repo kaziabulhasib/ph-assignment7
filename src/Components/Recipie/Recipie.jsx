089d158552108fec5489587c55661ed415f23869
kaziabulhasib <abulhasibkazi@gmail.com>
2024-03-16 22:46:55 +0530

    recipies section dynamic

--- src/Components/Recipie/Recipie.jsx
+++ src/Components/Recipie/Recipie.jsx
@@ -1,33 +1,41 @@
-const Recipie = () => {
+const Recipie = ({ recipie }) => {
+  const {
+    recipe_name,
+    short_description,
+    ingredients,
+    recipe_image,
+    preparing_time,
+    calories,
+  } = recipie;
   return (
     <div className='w-[380px] border p-6 rounded-2xl'>
       <img
         className='w-[330px] h-[200px] rounded-2xl mb-6'
-        src='images/Chicken Stir-Fry.jpg'
+        src={recipe_image}
         alt=''
       />
-      <h1 className='font-semibold text-xl mb-5'>Spaghetti Bolognese </h1>
+      <h1 className='font-semibold text-xl mb-5'>{recipe_name} </h1>
       <p className='border-b-2 text-[#878787] pb-4 mb-6 '>
-        Classic Italian pasta dish with savory meat sauce.
+        {short_description}
       </p>
       <div>
         <h1 className='text-[18px] font-medium text-[#282828] text-left mb-4'>
-          Ingredients: 6
+          Ingredients: {ingredients.length}
         </h1>
         <ul className='list-disc ml-4 border-b-2 pb-4 '>
-          <li>500g ground beef</li>
-          <li>1 onion, chopped</li>
-          <li>500g ground beef</li>
+          {ingredients.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
       </div>
       <div className='flex gap-4 my-6'>
         <div className='flex gap-1 text-[#282828CC]'>
           <h1>time:</h1>
-          <h3>30 minitues</h3>
+          <h3>{preparing_time}</h3>
         </div>
         <div className='flex gap-1 text-[#282828CC]'>
           <h1>Calories:</h1>
-          <h3>600 calories</h3>
+          <h3>{calories}</h3>
         </div>
       </div>
       <button className='bg-[#0BE58A] py-3 px-6 rounded-3xl text-[18px] text-[#150B2B] font-medium'>
--- src/Components/Recipies/Recipies.jsx
+++ src/Components/Recipies/Recipies.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 import Recipie from "../Recipie/Recipie";
 const Recipies = () => {
+  const [recipies, setRecipies] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setRecipies(data));
+  }, []);
   return (
-    <div>
-      <Recipie></Recipie>
+    <div className='grid grid-cols-2 gap-6'>
+      {recipies.map((recpie) => (
+        <Recipie key={recpie.recipe_id} recipie={recpie}></Recipie>
+      ))}
     </div>
   );
 };
